05558b744955861050e704b0a4313cd04cb82c6c
Paulo Cesar - Amor e Patas <skedarmusic@gmail.com>
2026-02-10 23:32:37 -0300

    feat: add google analytics ga4 script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Script from 'next/script'
 import { businessInfo } from '@/data/businessInfo'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -58,6 +59,19 @@ export default function RootLayout({
                     {children}
                 </div>
                 <Footer />
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-ME8S68TVBG"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+
+                        gtag('config', 'G-ME8S68TVBG');
+                    `}
+                </Script>
             </body>
         </html >
     )
